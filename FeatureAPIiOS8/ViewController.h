f29c655edb2400f3f6b180f85e811f2a0d2cf781
CyonLeu <gzliuyong2014@corp.netease.com>
2014-11-19 10:09:35 +0800

    Initial Commit

---
+++ FeatureAPIiOS8/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeatureAPIiOS8
+//
+//  Created by Liuyong on 14-11-19.
+//  Copyright (c) 2014年 Liuyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FeatureAPIiOS8/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FeatureAPIiOS8
+//
+//  Created by Liuyong on 14-11-19.
+//  Copyright (c) 2014年 Liuyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
