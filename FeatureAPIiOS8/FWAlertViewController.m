a60aac52ffe79c4db5fadb3025bd2dab486ca2c5
CyonLeu <gzliuyong2014@corp.netease.com>
2014-11-19 20:57:44 +0800

    first commit: learn about UIAlertController, UIVisualEffectView

---
+++ FeatureAPIiOS8/FWAlertViewController.m
@@ -0,0 +1,37 @@
+//
+//  FWAlertViewController.m
+//  FeatureAPIiOS8
+//
+//  Created by Liuyong on 14-11-19.
+//  Copyright (c) 2014年 Liuyong. All rights reserved.
+//
+
+#import "FWAlertViewController.h"
+
+@interface FWAlertViewController ()
+
+@end
+
+@implementation FWAlertViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ FeatureAPIiOS8/FWBlurEffectViewController.h
@@ -0,0 +1,13 @@
+//
+//  FWBlurEffectViewController.h
+//  FeatureAPIiOS8
+//
+//  Created by Liuyong on 14-11-19.
+//  Copyright (c) 2014年 Liuyong. All rights reserved.
+//
+
+#import "FWBaseViewController.h"
+
+@interface FWBlurEffectViewController : FWBaseViewController
+
+@end
